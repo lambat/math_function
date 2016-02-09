5932df7da657e7014a9b341e88f0e669c177ab07
Zubair Lambat <P14149985@GWHXX583LIN008W.lec-admin.dmu.ac.uk>
2016-02-09 11:14:54 +0000

    Committing a maths file to branch with

---
+++ maths_branching/maths_function.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+float add (float x, float y)
+
+{
+    return x + y;
+}
+
+float multiply (float x, float y)
+
+{
+    return x * y;
+}
+
+float divide (float x, float y)
+
+{
+    return x / y;
+}
+
+float subtract (float x, float y)
+{
+    return x - y;
+}
+
+float minimum (float x, float y)
+{
+	if(x>y)         
+    return y;
+}
+
+float maximum (float x, float y)
+{
+	if(x<y)         
+    return y;
+}
+
+int main()
+{
+    float a, b, r;
+    char op;
+    do {
+       printf("number  op  number?  ");
+       scanf(" %f %c %f", &a, &op, &b);
+       switch (op)
+       {
+           case '+' : r = add(a,b);break;
+           case '*' : r = multiply(a,b);break;
+           case '-' : r = subtract(a,b);break;
+           case '/' : r = divide(a,b);break;
+           case 'm' : r = minimum(a,b);break;
+           case 'M' : r = maximum(a,b);break;
+           case 'q' : break;
+           default  : op='?';
+       }
+       if (op=='?')
+          printf("Unknown operator\n");
+       else if (op=='q')
+          printf("Bye :)\n");
+       else
+          printf("%f %c %f = %f\n", a, op, b, r);
+    }
+    while (op != 'q');
+    
+    return 0;
+}
